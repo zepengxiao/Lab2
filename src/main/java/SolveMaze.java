d970709ca294077d5fffaf9d46653bc632b71931
zepengxiao <zepengx2@illinois.edu>
2018-01-31 16:07:58 -0600

    Finished maze.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,6 +34,25 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
+            while (!maze.isFinished()){
+                maze.turnLeft();
+                if (!maze.canMove()) {
+                    maze.turnRight();
+                    if (!maze.canMove()) {
+                        maze.turnRight();
+                        if (!maze.canMove()) {
+                            maze.turnRight();
+                            maze.move();
+                        } else {
+                            maze.move();
+                        }
+                    } else{
+                        maze.move();
+                    }
+                } else {
+                    maze.move();
+                }
+            }
             // Implement your maze solving algorithm here
         }
 
